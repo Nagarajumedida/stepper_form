c4187b0cc60a2d16dbb7ae8cf21cd934d2c0c906
Medidanagaraju <nagarajumedida123@gmail.com>
2023-03-15 16:54:29 +0530

    Stepper Created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 
+import { Step, StepLabel, Stepper } from '@material-ui/core';
 import './App.css';
+import Address from './Components/Address';
+import FinalData from './Components/FinalData';
+import UserInformation from './Components/UserInformation';
 
 function App() {
+
+  const stepperDetails = [
+    { label: "User Information" },
+    { label: "Address Details" },
+    { label: "Thank You" },
+
+
+  ]
   return (
     <div className="App">
-     Steeper FOrm
+      <h3 style={{ color: "red", textDecoration: "underline" }}>Stepper User Details Form</h3>
+      <div className='center-stepper'>
+
+        <Stepper alternativeLabel>
+          {stepperDetails.map((item, i) => (
+            <Step key={i}>
+              <StepLabel>{item.label}</StepLabel>
+            </Step>
+          ))}
+        </Stepper>
+      </div>
+
+
+
+
     </div>
   );
 }
